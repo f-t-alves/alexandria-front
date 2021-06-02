7b630f7d91234ad8325d24aaf0fce01dd1161cb1
Fabio T Alves <ftalves@poli.ufrj.br>
2021-06-02 14:22:10 -0300

    Using API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api/movie")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={!data ? logo : data.poster} id={!data ? "App-logo" : "movie-poster"} alt="logo" />
+        <p id="movie-title">{!data ? "Loading movie" : data.title}</p>
+        <p id="fullplot">{!data ? "" : data.fullplot}</p>
       </header>
     </div>
   );
